a630be721cbaaf2af679eb7549c3e7f1cdc204b9
Raja Soundaramourty <rajasoun@icloud.com>
2021-06-10 19:30:46 +0530

    TDD through Pester

---
+++ src/lib/core.Tests.ps1
@@ -0,0 +1,16 @@
+BeforeAll { 
+    . $PSScriptRoot/core.ps1
+    . $PSCommandPath.Replace('.Tests.ps1','.ps1')
+}
+
+Describe 'Test core.ps1' {
+    Context "unit tests" -Tag "unit" {
+        It "Returns <expected> (<name>)" -ForEach @(
+            @{ Name = "Pester"; Expected = 'True'}
+            @{ Name = "PSReadLine"; Expected = 'True'} 
+            @{ Name = "git"; Expected = 'False'} 
+        ) {
+            Test-ModuleAvailable -Name $name | Should -Be $expected
+        }
+    }
+}
