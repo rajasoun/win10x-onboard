280ade1362c6dc4acd44a27a0311e636e5e021ce
raja Soundaramourty <rajasoun@icloud.com>
2022-05-16 01:50:31 +0530

    feat(oh-my-bash): teardown

--- nix/assist.sh
+++ nix/assist.sh
@@ -16,15 +16,25 @@ case ${choice} in
       echo -e "${GREEN}\nRun${NC}"
       echo -e "${ORANGE}source <(curl -s source "https://raw.githubusercontent.com/rajasoun/common-lib/main/docker/wrapper.sh")${NC}"
     ;;
-    "setup")
+    "bash-setup")
       if [ ! -d ~/.oh-my-bash ];then 
         bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
-        cp dotfiles/.bashrc ~/.bashrc
-        cp dotfiles/.bash_profile ~/.bash_profile
+        yes | cp dotfiles/.bashrc ~/.bashrc
+        yes | cp dotfiles/.bash_profile ~/.bash_profile
         echo -e "\n oh-my-bash Installation Done !!!"
       else 
         echo -e "\n oh-my-bash Already Installed !!!"
         echo -e "${ORANGE}Remove ~/.oh-my-bash if you want to re-install${NC}"
+      fi
+    ;;
+    "bash-teardown")
+      if [ -d ~/.oh-my-bash ];then 
+        rm -fr ~/.oh-my-bash/
+        rm -fr ~/.bashrc
+        rm -fr ~/.bash_profile 
+        echo -e "\n oh-my-bash UnInstallation Done !!!"
+      else 
+        echo -e "\n oh-my-bash Already UnInstalled !!!"
       fi
     ;;
     *)
